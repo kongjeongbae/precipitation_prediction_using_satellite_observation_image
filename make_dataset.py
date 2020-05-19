a8d5403704ed0a95c1e6624d8159ebb24686071a
kongjeongbae <kongjeongbae@gmail.com>
2020-05-19 14:04:04 +0900

    데이터셋 부풀리는 중, 비가 특이하게 많이 내린 사진들 더 넣고 있음

--- make_dataset.py
+++ make_dataset.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 dir_train = 'inputs/train/'
 dir_test = 'inputs/test/'
 
+
 def original_set(dir_train, dir_test):
     UPPER = 100
     # train dataset
@@ -14,13 +15,13 @@ def original_set(dir_train, dir_test):
 
     for i in os.listdir(dir_train):
         npy = np.load(dir_train + i)
-        
+
         # missing value 제거
-        if npy[:,:,-1].sum() < 0:
+        if npy[:, :, -1].sum() < 0:
             continue
-        if (npy[:,:,-1] >= 0.1).sum() >= UPPER:
-            train.append(npy[:,:,:-1])
-            train_y.append(npy[:,:,-1])
+        if (npy[:, :, -1] >= 0.1).sum() >= UPPER:
+            train.append(npy[:, :, :-1])
+            train_y.append(npy[:, :, -1])
 
     train = np.array(train)
     train_y = np.array(train_y)
@@ -46,6 +47,36 @@ def original_set(dir_train, dir_test):
     #     pickle.dump(test, f, protocol=4)
     # del test
 
+
+def many_rain(dir_train, UPPER):
+    # UPPER 이상 강수량을 기록한 픽셀이 존재하는 사진만 골라내는 코드
+    train = []
+    train_y = []
+
+    for i in tqdm(os.listdir(dir_train)):
+        npy = np.load(dir_train + i)
+
+        # missing value 제거
+        if npy[:, :, -1].sum() < 0:
+            continue
+        if (npy[:, :, -1] >= UPPER).sum() >= 1:
+            train.append(npy[:, :, :-1])
+            train_y.append(npy[:, :, -1])
+
+    train = np.array(train)
+    train_y = np.array(train_y)
+
+    with open(f'inputs/train_{UPPER}up.pickle', 'wb') as f:
+        pickle.dump(train, f, protocol=4)
+
+    with open(f'inputs/train_y_{UPPER}up.pickle', 'wb') as f:
+        pickle.dump(train_y, f, protocol=4)
+
+    print(len(train))
+    del train
+    del train_y
+
+
 def tree_set():
     dir_train = 'inputs/train/tmp/'
     # train dataset
@@ -54,12 +85,12 @@ def tree_set():
 
     for i in tqdm(os.listdir(dir_train)):
         npy = np.load(dir_train + i)
-        
+
         # missing value 제거
-        if npy[:,:,-1].sum() < 0:
+        if npy[:, :, -1].sum() < 0:
             continue
-        train.append(npy[:,:,:-1])
-        train_y.append(npy[:,:,-1])
+        train.append(npy[:, :, :-1])
+        train_y.append(npy[:, :, -1])
 
     train = np.array(train)
     train_y = np.array(train_y)
@@ -73,28 +104,30 @@ def tree_set():
     del train
     del train_y
 
+
 def augmentation(dir_train):
 
-# Augmentation (rotation 데이터 만들기)
-# - 데이터에 강수량이 존재하는 자료만 rotation 자료 만들 것 - 73000여개 중 61849개에서 비가 옴
-# - 해당 사진에서 평균적으로 0.1 이상인 것만 rotation 하거나 (대회 평가에서 0.1 이상인 픽셀만 사용한다고 했음)
-# - 1600픽셀 중 강수량이 0.1 이상인 픽셀이 100개 이상인 사진만 로테이션 시켜도 되는데 우선은 후자로 테스트 해보자.
+    # Augmentation (rotation 데이터 만들기)
+    # - 데이터에 강수량이 존재하는 자료만 rotation 자료 만들 것 - 73000여개 중 61849개에서 비가 옴
+    # - 해당 사진에서 평균적으로 0.1 이상인 것만 rotation 하거나 (대회 평가에서 0.1 이상인 픽셀만 사용한다고 했음)
+    # - 1600픽셀 중 강수량이 0.1 이상인 픽셀이 100개 이상인 사진만 로테이션 시켜도 되는데 우선은 후자로 테스트 해보자.
 
     rot_train = []
     rot_train_y = []
 
     for i in tqdm(os.listdir(dir_train)):
         npy = np.load(dir_train + i)
-        
-        if npy[:,:,-1].sum() < 0:
+
+        if npy[:, :, -1].sum() < 0:
             continue
-        
+
         # 1600 픽셀 사진에서 강수량이 0.1 이상인 픽셀이 100개 이상인 사진만 로테이션 시켜보자.
-        if (npy[:,:,-1] >= 0.1).sum() > 100:
-            npy = np.rot90(npy, 3, (0,1)) # 2번째인자가 1이면 90도, 2이면 180도, 3번째 인자는 어떤 축을 잡고 돌릴것인지, 0번축, 1번축을 기준으로 돌릴것이다
-            rot_train.append(npy[:,:,:-1])
-            rot_train_y.append(npy[:,:,-1])
-        
+        if (npy[:, :, -1] >= 0.1).sum() > 100:
+            # 2번째인자가 1이면 90도, 2이면 180도, 3번째 인자는 어떤 축을 잡고 돌릴것인지, 0번축, 1번축을 기준으로 돌릴것이다
+            npy = np.rot90(npy, 3, (0, 1))
+            rot_train.append(npy[:, :, :-1])
+            rot_train_y.append(npy[:, :, -1])
+
     rot_train = np.array(rot_train)
     rot_train_y = np.array(rot_train_y)
 
@@ -103,17 +136,17 @@ def augmentation(dir_train):
 
     with open('inputs/rot_train270_y.pickle', 'wb') as f:
         pickle.dump(rot_train_y, f, protocol=4)
-    
+
     del rot_train
     del rot_train_y
-    
+
 
 def augmentation_to_npy(dir_train):
 
-# Augmentation (rotation 데이터 만들기)
-# - 데이터에 강수량이 존재하는 자료만 rotation 자료 만들 것 - 73000여개 중 61849개에서 비가 옴
-# - 해당 사진에서 평균적으로 0.1 이상인 것만 rotation 하거나 (대회 평가에서 0.1 이상인 픽셀만 사용한다고 했음)
-# - 1600픽셀 중 강수량이 0.1 이상인 픽셀이 100개 이상인 사진만 로테이션 시켜도 되는데 우선은 후자로 테스트 해보자.
+    # Augmentation (rotation 데이터 만들기)
+    # - 데이터에 강수량이 존재하는 자료만 rotation 자료 만들 것 - 73000여개 중 61849개에서 비가 옴
+    # - 해당 사진에서 평균적으로 0.1 이상인 것만 rotation 하거나 (대회 평가에서 0.1 이상인 픽셀만 사용한다고 했음)
+    # - 1600픽셀 중 강수량이 0.1 이상인 픽셀이 100개 이상인 사진만 로테이션 시켜도 되는데 우선은 후자로 테스트 해보자.
 
     rot = {
         1: '90',
@@ -123,22 +156,24 @@ def augmentation_to_npy(dir_train):
 
     for i in tqdm(os.listdir(dir_train)):
         npy = np.load(dir_train + i)
-        
-        if npy[:,:,-1].sum() < 0:
+
+        if npy[:, :, -1].sum() < 0:
             continue
-        
+
         # 1600 픽셀 사진에서 강수량이 0.1 이상인 픽셀이 100개 이상인 사진만 로테이션 시켜보자.
-        if (npy[:,:,-1] >= 0.1).sum() > 100:
+        if (npy[:, :, -1] >= 0.1).sum() > 100:
             nplr = np.fliplr(npy)
             npud = np.flipud(npy)
             np.save(f'inputs/rot/{i}_lr.npy', nplr)
             np.save(f'inputs/rot/{i}_ud.npy', npud)
             for angle in range(1, 4):
-                npy_ = np.rot90(npy, angle, (0,1)) # 2번째인자가 1이면 90도, 2이면 180도, 3번째 인자는 어떤 축을 잡고 돌릴것인지, 0번축, 1번축을 기준으로 돌릴것이다
+                # 2번째인자가 1이면 90도, 2이면 180도, 3번째 인자는 어떤 축을 잡고 돌릴것인지, 0번축, 1번축을 기준으로 돌릴것이다
+                npy_ = np.rot90(npy, angle, (0, 1))
                 np.save(f'inputs/rot/{i}_{rot[angle]}.npy', npy_)
-            
 
-original_set(dir_train, dir_test)
+
+# original_set(dir_train, dir_test)
+many_rain(dir_train, 250)
 # augmentation(dir_train)
 # tree_set()
 # augmentation_to_npy(dir_train)
--- preprocessing.py
+++ preprocessing.py
@@ -41,3 +41,36 @@ def scaling(train, test, how):
     test = test.reshape(TE_SHAPE_0 , TE_SHAPE_1 , TE_SHAPE_2, TE_SHAPE_3)
 
     return train, test
+
+
+def rotation(data):
+    v1_m_h1 = data[:, :, :, 0] * np.cos(np.pi / 4) - data[:, :, :, 1] * np.sin(np.pi / 4)  
+    v1_p_h1 = data[:, :, :, 0] * np.cos(np.pi / 4) + data[:, :, :, 1] * np.sin(np.pi / 4)  
+    data[:, :, :, 0] = v1_m_h1
+    data[:, :, :, 1] = v1_p_h1 
+    del v1_m_h1
+    del v1_p_h1
+
+    v2_m_h2 = data[:, :, :, 2] * np.cos(np.pi / 4) - data[:, :, :, 3] * np.sin(np.pi / 4)
+    v2_p_h2 = data[:, :, :, 2] * np.cos(np.pi / 4) + data[:, :, :, 3] * np.sin(np.pi / 4)
+    data[:, :, :, 2] = v2_m_h2
+    data[:, :, :, 3] = v2_p_h2 
+    del v2_m_h2
+    del v2_p_h2
+    
+    v4_p_h4_30 = data[:, :, :, 5] * np.cos(np.pi / 4) + data[:, :, :, 6] * np.sin(np.pi / 4)
+    v4_m_h4_30 = data[:, :, :, 5] * np.cos(np.pi / 4) - data[:, :, :, 6] * np.sin(np.pi / 4)
+    data[:, :, :, 5] = v4_p_h4_30
+    data[:, :, :, 6] = v4_m_h4_30
+
+    v5_p_h5_30 = data[:, :, :, 7] * np.cos(np.pi / 6) + data[:, :, :, 8] * np.sin(np.pi / 6)
+    v5_m_h5_30 = data[:, :, :, 7] * np.cos(np.pi / 6) - data[:, :, :, 8] * np.sin(np.pi / 6)
+    data[:, :, :, 7] = v5_p_h5_30
+    data[:, :, :, 8] = v5_m_h5_30
+
+    del v4_p_h4_30
+    del v4_m_h4_30
+    del v5_p_h5_30
+    del v5_m_h5_30
+    
+    return data
